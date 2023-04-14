df60d93588492c7d1a183e484561d0dedb8e7c63
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2023-04-14 21:13:33 +0530

    code formatting

--- script.js
+++ script.js
@@ -6,46 +6,45 @@ const noOfNotes = document.querySelectorAll(".NoOfNotes");
 
 var message = document.querySelector("#error-message")
 
-const avilabelNotes = [2000,500,200,100,50,20,10,5,1];
+const avilabelNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
 button.addEventListener("click", CheckAmount);
 
-function CheckAmount()
-{
+function CheckAmount() {
     hideMessage();
 
-    if(billAmount.value > 0)
-    {
-       if(Number(cashGiven.value) > Number(billAmount.value)){
-          const AmountToBeReturn = cashGiven.value - billAmount.value;
-          CalculateChange(AmountToBeReturn);
-          
-       }
-       else{
-           errorMessage("No money to be returned")
-       }
-    } 
-    else{
-      errorMessage("Bill amount should be greater than zero")
-    }  
+    if (billAmount.value > 0) {
+        if (Number(cashGiven.value) > Number(billAmount.value)) {
+            const AmountToBeReturn = cashGiven.value - billAmount.value;
+            CalculateChange(AmountToBeReturn);
+
+        } else {
+            errorMessage("No money to be returned")
+        }
+    } else {
+        errorMessage("Bill amount should be greater than zero")
+    }
 }
-function hideMessage(){
+
+function hideMessage() {
     message.style.display = "none";
 
 }
-function errorMessage(msg){
-    message.style.display="block";
+
+function errorMessage(msg) {
+    message.style.display = "block";
     message.innerText = msg;
 
 }
-function CalculateChange(AmountToBeReturn){
-    
-    for(let i = 0; i < avilabelNotes.length; i++){
-        const Notes = Math.trunc( AmountToBeReturn / avilabelNotes[i]);
+
+function CalculateChange(AmountToBeReturn) {
+
+    for (let i = 0; i < avilabelNotes.length; i++) {
+        const Notes = Math.trunc(AmountToBeReturn / avilabelNotes[i]);
 
         AmountToBeReturn = AmountToBeReturn % avilabelNotes[i];
         noOfNotes[i].innerText = Notes;
-      
+
     };
-   
-  
+
+
 }
